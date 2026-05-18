358faec954ab0cc1e48880c894a1ee7e786ff302
Dhananjoy <dhananjoybrahma333@gmail.com>
2026-05-18 23:04:32 +0530

    login conditionally render username if logged in

--- JWT/Backend-JWT/middleware/authMiddleware.js
+++ JWT/Backend-JWT/middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import {User} from "../models/Users.js";
 
 const requireAuth = (req, res, next) => {
     const token = req.cookies.jwt
@@ -16,6 +17,29 @@ const requireAuth = (req, res, next) => {
         })
     }else{
         res.status(401).json({error: 'No token provided'});
+    }
+}
+
+// check current user
+export const checkUser = (req, res, next) => {
+    const token = req.cookies.jwt
+
+    if(token){
+        jwt.verify(token, process.env.JWT_SECRET, async (err, decodedToken) => {
+            if(err){
+                console.log(err.message)
+                res.locals.user = null
+                next()
+            }else {
+                console.log(decodedToken);
+                let user = await User.findById(decodedToken.id)
+                res.locals.user = user
+                next()
+            }
+        })
+    }else{
+        res.locals.user = null
+        next()
     }
 }
 
